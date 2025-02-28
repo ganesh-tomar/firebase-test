299754ff640f8bdabd057d42dfe21c0add493b7e
Ganesh <ganesh@cleardigital.com>
2025-02-28 17:59:34 +0530

    update firebase push function

--- app/page.js
+++ app/page.js
@@ -1,11 +1,26 @@
 "use client";
-import { get, ref } from "firebase/database";
+import { get, push, ref, set } from "firebase/database";
 import { useEffect, useState } from "react";
 import { database } from "./firebaseConfig";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
+  const [title, setTitle] = useState("");
   console.log(users);
+
+  const handleAddData = () => {
+    try {
+      const usersRef = ref(database, "users");
+      const newDataRef = push(usersRef);
+      set(newDataRef, {
+        title: title,
+      });
+      setTitle("");
+      alert("Data Added Successfully!!");
+    } catch (error) {
+      console.error("Firebase Error: ", error);
+    }
+  };
 
   useEffect(() => {
     const useRef = ref(database, "users");
@@ -31,14 +46,31 @@ export default function Home() {
   return (
     <div className="conatiner mx-auto max-w-[1600px]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Fetch Data</h1>
-        <div>
+        {/* <h1>Fetch Data</h1> */}
+        {/* <div>
           {users &&
             users.map((user) => (
               <li key={user.id} className="">
                 <p className="text-cosmos">{user.title}</p>
               </li>
             ))}
+        </div> */}
+
+        <h1>Add Data</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full border p-4"
+          />
+          <button
+            onClick={handleAddData}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Add Data
+          </button>
         </div>
       </main>
     </div>
